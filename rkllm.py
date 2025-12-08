4ade6d6328498548e942017f5db304bef0ff4cf7
hiroi <dukihiroi@gmail.com>
2025-12-08 23:58:16 +0900

    remove old code

--- rkllm.py
+++ rkllm.py
@@ -4,8 +4,6 @@ import os
 import time
 # from transformers import AutoTokenizer
 
-PROMPT_TEXT_PREFIX = ""
-PROMPT_TEXT_POSTFIX = " <｜Assistant｜>"
 # 使用Tokenizer自动应用模板
 # tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
 
